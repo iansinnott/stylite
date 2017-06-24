c4602ba5e03838e1846003f968253e5252dc7295
Ian Sinnott <ian@iansinnott.com>
2017-06-23 21:39:04 -0700

    Create popup that stores rext by hostname

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ A lightweight style editor. Apply any styles you want to any site.
 ## Why?
 
 * Many sites have styling issues. Small text, low contrast, useless sidebars, etc. Now you can fix that.
+
+## Reference
+
+* [Permissions][]
+
+[Permissions]: https://developer.chrome.com/extensions/declare_permissions
+
+## TODO
+
+* Save on close tab/modal
+* Improve formatting of textarea
--- popup.js
+++ popup.js
@@ -30,18 +30,65 @@ const getHostname = (url) => {
   return new URL(url).hostname;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const append = el => document.body.appendChild(el);
-  setTimeout(() => {
+const promisify = (fn) => (...args) => new Promise((resolve, reject) => {
+  fn(...args, (...result) => {
+    if (chrome.runtime.lastError) {
+      reject(chrome.runtime.lastError);
+      return;
+    }
+
+    resolve(...result);
+  });
+});
+
+const wrapStorage = (storage) => ({
+  get: promisify(storage.get),
+  set: promisify(storage.set),
+});
+
+const appendTo = target => el => target.appendChild(el);
+
+const run = () => {
+  const append = appendTo(document.body);
+  const storage = wrapStorage(chrome.storage.sync);
+  const normalizeId = (tab) => getHostname(tab.url); // Use host as id
+  const getStateById = (id) => {
+    return storage.get(id)
+      .then((result = {}) => result[id] || { id }); // Default to object with id
+  };
+  const renderDOM = (state) => {
+    const { id, contentState } = state;
+    console.log('Loaded STATE:', state);
+
     const subhead = document.createElement('h2');
     subhead.className = 'subhead';
-    subhead.textContent = 'Subhead Loaded'
+    subhead.textContent = `Found results for: ${id}`
     append(subhead);
 
-    getCurrentTab().then(tab => {
-      const p = document.createElement('p');
-      p.textContent = getHostname(tab.url);
-      append(p);
+    const editor = document.createElement('textarea');
+    editor.placeholder = 'Styles...'
+    editor.value = state.contentState || '';
+    append(editor);
+
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save'
+    saveButton.onclick = () => {
+      if (!editor.value) return; // Do nothing if no styles were saved
+      storage.set({ [id]: { id, contentState: editor.value } })
+        .then(x => console.log('saved', x), err => console.error('oh no', err))
+    };
+    append(saveButton);
+  };
+
+  Promise.resolve()
+    .then(getCurrentTab)
+    .then(normalizeId)
+    .then(getStateById)
+    .then(renderDOM)
+    .catch(err => {
+      console.warn('Could not fetch storage');
+      console.warn(err);
     });
-  }, 10);
-});
+};
+
+document.addEventListener('DOMContentLoaded', run);
