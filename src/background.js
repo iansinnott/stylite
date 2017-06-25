391810bc78a973003133a87c10f3d0455ba153d5
Ian Sinnott <ian@iansinnott.com>
2017-06-24 17:43:53 -0700

    Load in styles on load

--- src/background.js
+++ src/background.js
@@ -1,12 +1,26 @@
-const FETCH_STYLES = 'stylist/FETCH_STYLES';
+import { FETCH_STYLES, FETCH_STYLES_SUCCESS } from './action.js';
+import { getStateById } from './utils.js';
 
-const handleMessage = (request, sender, sendRequest) => {
+const getStyles = (state) => state.contentState || '';
+
+/**
+ * NOTE: Return true for async. If you want async behavior you MUST return true,
+ * otherwise chrome will invalided sendRequest and you will not get what you
+ * want.
+ */
+const handleMessage = (request = {}, sender, sendRequest) => {
   const { type, payload } = request;
 
   switch (type) {
   case FETCH_STYLES:
-    sendRequest({ styles: 'bleh' });
-    return;
+    Promise.resolve(payload.id)
+      .then(getStateById)
+      .then(getStyles)
+      .then(styles => sendRequest({
+        type: FETCH_STYLES_SUCCESS,
+        payload: { styles },
+      }));
+    return true; // IMPORTANT! See NOTE
   default:
     console.warn(`Unsupported type "${type}" passed in request`);
     return;
--- src/content.js
+++ src/content.js
@@ -1,13 +1,30 @@
-console.log('INIT content script');
+import {
+  fetchStyles,
+  FETCH_STYLES_SUCCESS,
+} from './action.js';
 
-const fetchStyles = () => ({
-  type: 'stylist/FETCH_STYLES',
-});
+const STYLE_TAG_SELECTOR='[data-stylite=true]';
 
-const handleMessage = (request, sender, sendRequest) => {
+const applyStyles = (styles) => {
+  let styleTag = document.querySelector(STYLE_TAG_SELECTOR);
+  if (styleTag) {
+    styleTag.textContent = styles;
+  } else {
+    styleTag = document.createElement('style');
+    styleTag.dataset.stylite = true;
+    styleTag.textContent = styles;
+    document.head.appendChild(styleTag);
+  }
+};
+
+const handleMessage = (request = {}, sender, sendRequest) => {
   const { type, payload } = request;
 
   switch (type) {
+  case FETCH_STYLES_SUCCESS:
+    console.warn('Fetched styles. Applying...', request);
+    applyStyles(payload.styles);
+    return;
   default:
     console.warn(`Content script just saw "${type}" passed in request`, request);
     return;
@@ -18,4 +35,4 @@ const handleMessage = (request, sender, sendRequest) => {
 chrome.runtime.onMessage.addListener(handleMessage);
 
 // initialize by fetching styles
-chrome.runtime.sendMessage(fetchStyles(), handleMessage);
+chrome.runtime.sendMessage(fetchStyles({ id: window.location.hostname }), handleMessage);
